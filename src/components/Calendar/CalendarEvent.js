f969cf63fe01ce41d5ece2d11ffaf5b99c2ae9fd
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-07-31 13:11:16 +0100

    updates to calendar

--- src/components/Calendar/CalendarEvent.js
+++ src/components/Calendar/CalendarEvent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const EventBlock = ({event}) => (
-    <a href={event.url}>
+    <a href={event.url} target='_blank' rel='noreferrer noopener'>
         <div className={`event-${event.availablity}`}>
             <div>{event.title}</div>
         </div>
