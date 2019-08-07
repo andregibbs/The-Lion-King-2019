1fa9b0b641951de719a47e37a48769ed5b1386e0
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-08-07 13:24:45 +0100

    interstitial update

--- src/components/Calendar/CalendarEvent.js
+++ src/components/Calendar/CalendarEvent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const EventBlock = ({event}) => (
-    <a href={event.url} target='_blank' rel='noopener noreferrer'>
+    <a href={event.url} className="interstitial-timed-bristol" target='_blank' rel='noopener noreferrer'>
         <div className={`event-${event.availablity}`}>
             <div>{event.title}</div>
         </div>
