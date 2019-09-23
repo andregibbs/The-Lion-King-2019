6927022e1ffa08dc92705bb24cdefbf6177f21c7
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-09-23 13:15:20 +0100

    updates to houseseats

--- src/templates/HouseSeatsTemplate.js
+++ src/templates/HouseSeatsTemplate.js
@@ -6,7 +6,7 @@ import {
 } from 'reactstrap';
 
 import Layout from 'components/Layout/Layout'
-import HouseSeatsForm from 'components/HouseSeatsForm/HouseSeatsForm'
+// import HouseSeatsForm from 'components/HouseSeatsForm/HouseSeatsForm'
 
 class HouseSeatsTemplate extends Component {
 
@@ -17,7 +17,7 @@ class HouseSeatsTemplate extends Component {
             <Layout data={data}>
                 <Container fluid className="bg-light-grey py-4">
                     <Row>
-                        <Col lg={6}>
+                        <Col xs={12}>
                             <div className="bg-white p-3 p-md-5">
                                 <h2 className="text-red font-special font-special--spacing">LONDON</h2>
                                 <p className="text-lg">The production holds a very limited number of top price seats for every performance, which are not available to the general public. Seats are subject to availability and requests will be reviewed and prioritised accordingly. Please note that filling out the request form is not a guarantee of tickets. Tickets are priced between £69.50 - £72.50 dependant on performance.</p>
@@ -26,16 +26,20 @@ class HouseSeatsTemplate extends Component {
                                 <p><strong>Evenings:</strong> Tuesday - Saturday at 7.30pm<br />
                                     <strong>Matinees:</strong> Wednesday, Saturday & Sunday at 2.30pm.</p>
                                 <hr className="hr-black my-4" />
-                                <p className="text-lg">To request House Seats for Disney's THE LION KING in London please complete the form. Once your request has been processed you will receive a confirmation email with reservation details. You will then need to call the box office and provide your credit card details to complete your booking. If you have any access requirements, please use the Additional Notes box.</p>
+                                <p className="text-lg">To request House Seats for Disney's THE LION KING in London please click the button below.</p>
+                                <div className="py-0">
+                                <a href="https://tickets.disney.co.uk/tickets/houseseats/TLKHOUSE?refId=cacC" class="btn btn--red my-4 no-interstitial-check">REQUEST HOUSE SEATS</a>
+                                </div>
+                                <p className="text-lg">If you have not requested house seats before, you will need to register. After completing the request form, it will be reviewed. If your request is successful, you will be sent a link to pay for your tickets online.</p>
                                 <p className="text-red"><strong>PLEASE NOTE THAT YOUR REQUEST MAY TAKE UP TO ONE WEEK PRIOR TO THE PERFORMANCE DATE TO BE CONFIRMED.</strong></p>
                                 <p><strong>Performance prices, dates and times subject to change without notice. Black-out periods may apply. Subject to availability. No refunds/exchanges.</strong></p>
                             </div>
                         </Col>
-                        <Col lg={6}>
+                        {/* <Col lg={6}>
                             <div className="bg-white p-3 p-md-5">
                                 <HouseSeatsForm />
                             </div>
-                        </Col>
+                        </Col> */}
                     </Row>
                 </Container>
             </Layout>
